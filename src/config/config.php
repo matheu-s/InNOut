50f555dd1e28e3af1cf8726bf4b37dfc444b6fcd
matheu-s <matheusboeni@gmail.com>
2022-03-24 23:15:10 +0200

    Implementing and testing model

---
+++ src/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once(realpath(dirname(__FILE__) . '/database.php'));
+date_default_timezone_set('Europe/Riga');
+setlocale('en-US', 'en-US.utf', 'english');
+
+//Folders
+define(MODEL_PATH, realpath(dirname(__DIR__) . '/../models'));
+
+
